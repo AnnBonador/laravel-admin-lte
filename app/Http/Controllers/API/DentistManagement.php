5edfc9215a1dbd07b23f2e063fd890b0cafb4ae6
rallion90 <ralphlaynes@gmail.com>
2022-12-30 23:43:51 +0400

    te

--- app/Http/Controllers/API/DentistManagement.php
+++ app/Http/Controllers/API/DentistManagement.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Controller as Controller;
 use Validator;
 use App\Models\User;
 use App\Models\ReviewRating;
+use App\Models\Treated;
 use Auth;
 class DentistManagement extends Controller
 {
@@ -19,6 +20,14 @@ class DentistManagement extends Controller
      */
     public function dentist_information(){
         $query = User::where('type', '=', 1)->get(['fname', 'lname', 'email', 'experience', 'image', 'specialization_id']);
+
+        return response()->json($query);
+    }
+
+    public function get_treated(){
+        $query = DB::table('treated')
+                ->join('appointment', 'treated.app_id', '=', 'appointment.id')
+                ->get();
 
         return response()->json($query);
     }
